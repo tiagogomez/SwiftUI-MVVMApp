eed9eba62b8b4cc7873cb4b8eb1b3ee24d204b98
santiagogomez09 <santiago.gomez@ceiba.com.co>
2022-04-11 18:42:41 -0500

    Initial Commit

---
+++ MVVMSwiftUI/MVVMSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMSwiftUIApp.swift
+//  MVVMSwiftUI
+//
+//  Created by Santiago Gómez Giraldo - Ceiba Software on 11/04/22.
+//
+
+import SwiftUI
+
+@main
+struct MVVMSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
